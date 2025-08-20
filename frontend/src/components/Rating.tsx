0bd607719cc62ea86db5215e5b33f194123141cd
unknown <zhur_zhur@ukr.net>
2025-08-20 20:04:36 +0300

    some bug with star

--- frontend/src/components/Rating.tsx
+++ frontend/src/components/Rating.tsx
@@ -1,9 +1,6 @@
-import {useState} from 'react';
 import {FaStar} from 'react-icons/fa'; // Requires 'react-icons' package
 
 const StarRating = ({startRating}: { startRating: number }) => {
-    const [rating, setRating] = useState(startRating); // State to store the selected rating
-    const [hover, setHover] = useState(rating);   // State to handle hover effect
     return (
         <div className="flex items-center">
             {[...Array(5)].map((_, index) => {
@@ -18,13 +15,13 @@ const StarRating = ({startRating}: { startRating: number }) => {
                         />
                         <FaStar
                             size={30}
-                            color={currentRating <= (hover || rating) ? '#ffc107' : '#e4e5e9'}
+                                color={currentRating <= startRating ? '#ffc107' : '#e4e5e9'}
                             style={{cursor: 'pointer'}}
                         />
                     </label>
                 );
             })}
-            <p>{rating}</p>
+            <p className="ml-2 text-gray-600">{startRating}</p>
         </div>
     );
 };
