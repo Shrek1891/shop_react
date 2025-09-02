84ff64d7f350846f92192107761e66e1c488fb4e
unknown <zhur_zhur@ukr.net>
2025-09-02 21:43:44 +0300

    setting simple jwt

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from .views import MyTokenObtainPairView
 
 urlpatterns = [
+    path('users/login', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', views.get_routes, name="routes"),
     path('products/', views.get_products, name="products"),
     path('products/<str:pk>/', views.get_product, name="product"),
--- backend/base/views.py
+++ backend/base/views.py
@@ -5,6 +5,20 @@ from .resources import products
 from rest_framework.decorators import api_view
 from .models import Product
 from .seraillizers import ProductSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 # Create your views here.
